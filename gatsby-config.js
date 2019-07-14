8a2456aca6a67d1dfa99fb319ca3bfa51e0d3319
vantezzen <properly@protonmail.com>
2019-07-14 18:39:18 +0200

    Fix manifest data

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Bennett Hollstein`,
+        short_name: `vantezzen`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#212121`,
+        theme_color: `#212121`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
