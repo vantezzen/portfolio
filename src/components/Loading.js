5eca83a2799f3c7814f814f1b9c3b4e10c1bb0e9
Bennett <hello@vantezzen.io>
2020-12-13 13:39:44 +0100

    Fix loading class

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,10 +3,10 @@ import './Loading.css';
 
 const Loading = () => (
   <div className="loading-container">
-    <div class="spinner">
-      <div class="bounce1"></div>
-      <div class="bounce2"></div>
-      <div class="bounce3"></div>
+    <div className="spinner">
+      <div className="bounce1"></div>
+      <div className="bounce2"></div>
+      <div className="bounce3"></div>
     </div>
   </div>
 );
