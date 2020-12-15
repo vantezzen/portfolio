49ac92ef8a6180231abe882bd47837478fb617bb
Bennett <hello@vantezzen.io>
2020-12-15 08:56:29 +0100

    Update hero-background.js

--- src/components/hero-background.js
+++ src/components/hero-background.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import loadable from '@loadable/component';
 import tw from 'tailwind.macro'
 import { RotateCw } from 'react-feather';
@@ -19,49 +19,91 @@ const backgrounds = [
   // breathingColor
 ];
 
-const HeroBackground = (props) => {
-  const [currentBackground, setBackground] = useState(Math.floor(Math.random() * backgrounds.length));
-  const background = backgrounds[currentBackground];
+class HeroBackground extends Component {
+  state = {
+    currentBackground: 0,
+    show: true
+  };
 
-  return (
-    <>
-      <div id="hero-background" style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-      }}>
-        <Sketch
-          {...background}
-          key={currentBackground}
-          setup={(...args) => {
-            background.setup(...args);
-            if (props.onLoad) {
-              props.onLoad();
-            }
+  constructor(props) {
+    super(props);
+
+    this.scrollListener = this.scrollListener.bind(this)
+  }
+
+  scrollListener() {
+    if (window.scrollY > 300 && this.state.show) {
+      this.setState({
+        show: false
+      });
+    } else if (window.scrollY < 300 && !this.state.show) {
+      this.setState({
+        show: true
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      // Use a random background
+      currentBackground: Math.floor(Math.random() * backgrounds.length),
+    });
+
+    window.addEventListener('scroll', this.scrollListener);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollListener);
+  }
+
+  render() {
+    const { show, currentBackground } = this.state;
+    const background = backgrounds[currentBackground];
+
+    return (
+      <>
+        <div id="hero-background" style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          ...show ? {} : { display: 'none' },
+          zIndex: 0
+        }}>
+          <Sketch
+            {...background}
+            key={currentBackground}
+            setup={(...args) => {
+              background.setup(...args);
+              if (this.props.onLoad) {
+                this.props.onLoad();
+              }
+            }}
+          />
+        </div>
+        <button 
+          style={{
+            bottom: '1rem',
+            right: '1rem',
+            ...tw`bg-transparent fixed z-10 border-none outline-none cursor-pointer`
           }}
-        />
-      </div>
-      <button 
-        style={{
-          bottom: '1rem',
-          right: '1rem',
-          ...tw`bg-transparent fixed z-10 border-none outline-none cursor-pointer`
-        }}
-        onClick={() => {
-          let newBackground = currentBackground + 1;
-          if (newBackground >= backgrounds.length) {
-            newBackground = 0;
-          }
+          onClick={() => {
+            let newBackground = currentBackground + 1;
+            if (newBackground >= backgrounds.length) {
+              newBackground = 0;
+            }
 
-          setBackground(newBackground);
-        }}
-        title="Get new background"
-        id="hero-background-btn"
-      >
-        <RotateCw />
-      </button>
-    </>
-  );
-};
+            this.setState({
+              currentBackground: newBackground,
+            });
+          }}
+          title="Get new background"
+          id="hero-background-btn"
+        >
+          <RotateCw />
+        </button>
+      </>
+    );
+  }
+}
 
 export default HeroBackground;
