d08c13b846d823463e9d56d3c370251111af45d5
Bennett <hello@vantezzen.io>
2023-07-04 17:37:16 +0200

    dev

--- src/components/landing/Contact.svelte
+++ src/components/landing/Contact.svelte
@@ -9,7 +9,13 @@
 
 <Section title="Contact">
 	<Card color="bg-orange-100">
-		<form name="contact" method="POST" data-netlify="true" action="/success" data-netlify-recaptcha="true">
+		<form
+			name="contact"
+			method="POST"
+			data-netlify="true"
+			action="/success"
+			data-netlify-recaptcha="true"
+		>
 			<div class="grid gap-4">
 				<div class="text-zinc-500 font-medium">
 					For bug reports and feature requests please instead use GitHub issues on the respective
@@ -19,7 +25,7 @@
 				<Input label="Name" name="name" />
 				<Textarea label="Message" name="message" />
 				<input type="hidden" name="form-name" value="contact" />
-				<div data-netlify-recaptcha="true"></div>
+				<div class="g-recaptcha" data-sitekey="6Le2OPUmAAAAAHL1_Fo5UOe06nuy4zDKA-4DNSDr" />
 
 				<Button type="submit" highlighted>Send</Button>
 			</div>
